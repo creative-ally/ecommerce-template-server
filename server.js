ec72bb45119f7098ad58cb023ac902a8a9bf63ec
muhammad-jiku <muhammadjiku364@gmail.com>
2022-10-10 21:22:38 +0600

    server side code structure added

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
-console.log('Welcome to the server side repository!');
+// dependecies
+require('dotenv').config();
+const express = require('express');
+
+// important variables
+const app = express();
+const port = process.env.PORT || 5000;
+
+// displaying default response
+app.get('/', (req, res) => {
+  res.send(
+    'Welcome to server of the ecommerce clone - developed by CreativeAlly'
+  );
+});
+
+// listening to the port
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
