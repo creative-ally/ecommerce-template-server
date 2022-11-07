007a05cd743ffc6f50632096c3e3ca5c306d955c
muhammad-jiku <muhammadjiku364@gmail.com>
2022-11-07 19:09:41 +0600

    route url modified

--- index.js
+++ index.js
@@ -50,3 +50,12 @@ app.use(errorHandler);
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
+
+// if express fail to handle any error for that there's global errorHandler
+process.on('unhandledRejection', (err) => {
+  console.log(err.name);
+  console.log(err.message);
+  app.close(() => {
+    process.exit(1);
+  });
+});
