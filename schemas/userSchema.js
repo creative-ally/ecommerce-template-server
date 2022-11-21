9e5591d5e2aca03cf22fa0d73580cc401567e033
muhammad-jiku <muhammadjiku364@gmail.com>
2022-11-21 11:52:13 +0600

    userSchema added

---
+++ schemas/userSchema.js
@@ -0,0 +1,28 @@
+// dependencies
+const mongoose = require('mongoose');
+
+// creating schema for users
+const userSchema = mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: [true, 'Please provide your username'],
+      trim: true,
+      minLength: [3, 'Name must be at least 3 letters'],
+      maxLength: [100, 'Name is too long'],
+    },
+    email: {
+      type: String,
+      required: [true, 'Please provide your email'],
+      unique: [true, 'This user is already in!'],
+    },
+    password: { type: String, required: true },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = userSchema;
