2099def8c1ba96f0a7d81be1bb02c3404ec09a29
muhammad-jiku <muhammadjiku364@gmail.com>
2022-11-30 10:58:25 +0600

    token validated for email authentication

--- controllers/authController.js
+++ controllers/authController.js
@@ -10,30 +10,35 @@ const signIn = async (req, res) => {
   const { email, password } = req.body;
 
   try {
-    const oldUser = await User.find({ email: email });
-    console.log(oldUser);
-    if (!oldUser)
+    const oldUser = await User.findOne({ email: email });
+
+    if (!oldUser) {
       return res.status(404).json({ message: 'User does not exist' });
+    } else {
+      const isPasswordCorrect = await bcrypt.compare(
+        password,
+        oldUser.password
+      );
 
-    const isPasswordCorrect = await bcrypt.compare(password, oldUser.password);
+      if (!isPasswordCorrect)
+        return res.status(400).json({ message: 'Something went wrong' });
 
-    if (!isPasswordCorrect)
-      return res.status(400).json({ message: 'Something went wrong' });
+      const token = jwt.sign(
+        { email: oldUser.email },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+          expiresIn: '86400s',
+        }
+      );
 
-    // const token = jwt.sign(
-    //   { email: oldUser.email },
-    //   process.env.ACCESS_TOKEN_SECRET,
-    //   {
-    //     expiresIn: '86400s',
-    //   }
-    // );
-
-    res.status(200).json({
-      message: 'User existence test passed successfully!!',
-      data: oldUser,
-      //   accessToken: token,
-    });
+      res.status(200).json({
+        message: 'User existence test passed successfully!!',
+        data: oldUser,
+        accessToken: token,
+      });
+    }
   } catch (err) {
+    console.log(err);
     res.status(500).json({ message: 'There is a server side error' });
   }
 };
@@ -43,33 +48,35 @@ const signUp = async (req, res) => {
   const { username, email, password } = req.body;
 
   try {
-    const oldUser = await User.find({ email: email });
+    const oldUser = await User.findOne({ email: email });
 
-    if (oldUser)
+    if (!oldUser) {
+      const hashedPassword = await bcrypt.hash(password, 12);
+
+      const newUser = new User({
+        username: username,
+        email: email,
+        password: hashedPassword,
+      });
+
+      const savedUser = await newUser.save();
+
+      const token = jwt.sign(
+        { email: savedUser.email },
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: '86400s' }
+      );
+
+      res.status(200).json({
+        message: 'User added successfully!!',
+        data: savedUser,
+        accessToken: token,
+      });
+    } else {
       return res.status(400).json({ message: 'User already exists' });
-
-    const hashedPassword = await bcrypt.hash(password, 12);
-
-    const newUser = new User({
-      username: username,
-      email: email,
-      password: hashedPassword,
-    });
-
-    const savedUser = await newUser.save();
-    console.log(savedUser);
-    // const token = jwt.sign(
-    //   { email: savedUser.email },
-    //   process.env.ACCESS_TOKEN_SECRET,
-    //   { expiresIn: '86400s' }
-    // );
-
-    res.status(200).json({
-      message: 'User added successfully!!',
-      data: savedUser,
-      //   accessToken: token,
-    });
-  } catch (error) {
+    }
+  } catch (err) {
+    console.log(err);
     res.status(500).json({ message: 'There is a server side error' });
   }
 };
