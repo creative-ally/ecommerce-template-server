0b18d41520cd7c6a500d38fdc7fab36daf98eb1c
muhammad-jiku <muhammadjiku364@gmail.com>
2022-12-13 13:55:33 +0600

    update cart item function added

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -19,6 +19,33 @@ const addToCart = async (req, res) => {
   }
 };
 
+//update item in the cart
+const updateCart = async (req, res) => {
+  const id = req.params.id;
+  const updatedCartItem = req.body;
+  const opts = { runValidators: true };
+
+  try {
+    await Cart.findByIdAndUpdate(
+      { _id: id },
+      {
+        $set: updatedCartItem,
+      },
+      {
+        new: true,
+        opts,
+      }
+    );
+    res.status(200).json({
+      message: 'Cart item updated successfully',
+      data: updatedCartItem,
+    });
+  } catch (err) {
+    res.status(500).json({ error: 'There is a server side error' });
+  }
+};
+
 module.exports = {
   addToCart,
+  updateCart,
 };
