433c3ebf49b44ce4ce199344feb63705eac2f7fb
muhammad-jiku <muhammadjiku364@gmail.com>
2022-12-13 19:58:58 +0600

    update order by id function is added

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -19,6 +19,33 @@ const addToOrder = async (req, res) => {
   }
 };
 
+// update order item
+const updateOrder = async (req, res) => {
+  const id = req.params.id;
+  const updatedOrderItem = req.body;
+  const opts = { runValidators: true };
+
+  try {
+    await Order.findByIdAndUpdate(
+      { _id: id },
+      {
+        $set: updatedOrderItem,
+      },
+      {
+        new: true,
+        opts,
+      }
+    );
+    res.status(200).json({
+      message: 'Order item updated successfully',
+      data: updatedOrderItem,
+    });
+  } catch (err) {
+    res.status(500).json({ error: 'There is a server side error' });
+  }
+};
+
 module.exports = {
   addToOrder,
+  updateOrder,
 };
